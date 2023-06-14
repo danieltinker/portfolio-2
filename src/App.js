4168b35575aba1b687eb3bab81205a6bc13c64b2
Daniel Baruch <danielb534@gmail.com>
2023-06-14 10:08:47 +0300

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars,faMugHot} from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from 'react';
 
+import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const [isActive, setIsActive] = useState(false); // Example hook state
 
   const toggelLinks = ()=>{
@@ -14,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <header>
-            <nav id='nav'>
+            <nav id='nav'  style={{ backgroundColor: isActive && windowWidth<555 ? 'rgba(200, 200, 200, 1)':null, paddingBottom: 28} }>
               <p className='logo'>db</p>
 
               <i id='bars' className='fas fa-bars'><FontAwesomeIcon onClick={()=>{toggelLinks()}} icon={faBars} /></i>
@@ -38,14 +53,14 @@ function App() {
 
             <div className='text-box'>
               <div className='socials'>
-              <a href='https://www.linkedin.com/in/daniel-baruch-75204b204/' style={{color:'black'}}>
+              <a href='https://www.linkedin.com/in/daniel-baruch-75204b204/' target='_blank' rel="noreferrer" style={{color:'black'}}>
               <i>
                 <FontAwesomeIcon icon={faLinkedin} />
               </i>
               </a>
-              <a href='https://github.com/danieltinker' style={{color:'black'}}>
+              <a href='https://github.com/danieltinker' target='_blank' rel="noreferrer" style={{color:'black'}}>
               <i>
-              <FontAwesomeIcon icon={faGithub} />
+              <FontAwesomeIcon icon={faGithub} id='git'/>
               </i>
               </a>
               </div>
@@ -53,6 +68,17 @@ function App() {
               <p>&lt;h2&gt; software developer &lt;/h2&gt; </p>
             </div>
         </header>
+
+
+
+
+
+       
+        <footer>
+            <span className='credits'>
+              Site made with &hearts; and    &nbsp;   <FontAwesomeIcon icon={faMugHot} bounce style={{fontSize:14}} />  &nbsp; by daniel baruch
+              </span> 
+          </footer>
     </div>
   );
 }
